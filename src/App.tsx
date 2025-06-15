8e5551f36d207928c316b70277311999e766b091
danilodg <danilo.gomes.dg91@gmail.com>
2025-06-15 10:16:00 -0300

    add material

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
+import { Box, Button, Typography, Paper } from "@mui/material";
+
 const App = () => {
-  return <h1>Olá, sou uma extensão!</h1>;
+  return (
+    <Box
+      sx={{
+        minHeight: "200px",
+        minWidth: "300px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        p: 2,
+        bgcolor: "background.default",
+      }}
+    >
+      <Paper elevation={3} sx={{ p: 2, textAlign: "center", width: "100%" }}>
+        <Typography variant="h6" gutterBottom>
+          Olá! 👋
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          Bem-vindo à extensão!
+        </Typography>
+        <Button variant="contained" color="primary">
+          Clique aqui
+        </Button>
+      </Paper>
+    </Box>
+  );
 };
 
 export default App;
