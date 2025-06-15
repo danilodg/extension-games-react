b995295209a7a689eef5aaa582ad6a0d94f14693
danilodg <danilo.gomes.dg91@gmail.com>
2025-06-15 09:47:05 -0300

    first extension

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <h1>Olá, sou uma extensão!</h1>;
+};
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = resolve(__filename, '..');
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, 'popup.html'),
+      }
+    }
+  }
+});
